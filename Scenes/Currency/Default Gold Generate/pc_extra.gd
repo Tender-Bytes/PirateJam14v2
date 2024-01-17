256f82dd4e5b7ad4e132f8a005985f8b0a92598a
Brian <brianchan98@hotmail.com>
2024-01-17 18:50:58 -0500

    created exponential cost for PCs

--- Scenes/Currency/Default Gold Generate/pc_extra.gd
+++ Scenes/Currency/Default Gold Generate/pc_extra.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 @onready var gold_gen = get_node("../default gold gen")
 @onready var money = get_node("../Money")
+@onready var button = get_node("Button")
 
 var pc_owned: int
+const BASE_COST = 5
+var cost
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pc_owned = 1
+	cost = BASE_COST
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +20,9 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	if money.amount >= 5:
+	if money.amount >= cost:
+		money.change_value.emit(-cost)
 		pc_owned += 1
+		cost = round(BASE_COST * pow(1.2, pc_owned))
+		button.set_text("Buy more computers: $" + str(cost))
 		gold_gen.add_pc.emit(pc_owned)
-		money.change_value.emit(-5)
