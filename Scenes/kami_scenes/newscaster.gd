deb4c7a846d94e467e07bee70d1e4f077d2072d0
theonekami <theonekamisama@gmail.com>
2024-01-17 12:15:59 +0530

    newscaster system fixed

--- Scenes/kami_scenes/newscaster.gd
+++ Scenes/kami_scenes/newscaster.gd
@@ -25,24 +25,31 @@ func _process(delta):
 	pass
 
 
+
 func change_status():
-	current_messages+=1
 	if current_messages ==1:
 		$ColorRect.color=Color("yellow")
 	elif current_messages ==2:
 		$ColorRect.color= Color("orange")
 	elif current_messages ==3:
 		$ColorRect.color= Color("Red")
-
-func change_status_back():
-	current_messages-=1
-	if current_messages ==1:
-		$ColorRect.color=Color("yellow")
-	elif current_messages ==2:
-		$ColorRect.color= Color("orange")
 	elif current_messages ==0:
 		$ColorRect.color= Color("green")
+
 
 func _on_button_pressed():
 	cycle()
 		
+
+
+func _on_progress_bar_converted_pop(pop):
+	if pop<12.5:
+		current_messages=0
+	elif pop<25.0:
+		current_messages=1
+	elif pop<37.5:
+		current_messages=2
+	elif pop<50.1:
+		current_messages=3
+	change_status()
+	 # Replace with function body.
