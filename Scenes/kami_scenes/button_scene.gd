c14cc58835f630915783f4f2a96db88bb38344a2
theonekami <theonekamisama@gmail.com>
2024-01-18 19:36:47 +0530

    extra

--- Scenes/kami_scenes/button_scene.gd
+++ Scenes/kami_scenes/button_scene.gd
@@ -1,5 +1,7 @@
 extends TextureButton
 @export var parti:Texture2D
+var og_size=scale 
+var g_size=Vector2(1.06,1.06)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,13 +17,19 @@ func _process(delta):
 
 func _on_mouse_entered():
 	if not(disabled):
+		grow_btn(g_size,.1)
 		$hover.show()
 
 
 
 func _on_hover_mouse_exited():
 	if not(disabled):
+		grow_btn(og_size,.1)
 		$hover.hide()
+
+func grow_btn(size:Vector2, duration:float):
+	var tween= create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self,'scale',size,duration)
 
 
 func _on_pressed():
