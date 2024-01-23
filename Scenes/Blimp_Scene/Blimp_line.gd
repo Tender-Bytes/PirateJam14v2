cb8f6647b765d95bd7e9494b20a5c87b3dcdd35b
Brian <brianchan98@hotmail.com>
2024-01-23 12:39:15 -0500

    created blimp overworld animation on blimp upgrade

---
+++ Scenes/Blimp_Scene/Blimp_line.gd
@@ -0,0 +1,37 @@
+extends Node2D
+@export var dir:int = -1
+@export var time:int = 15
+var blimp_scene = preload("res://Scenes/Blimp_Scene/Blimp.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func start_blimp():
+	spawn_blimp()
+	$Timer.wait_time = time
+	$Timer.start()
+	
+
+func spawn_blimp():
+	var blimp = blimp_scene.instantiate()
+	blimp.position.x = 0
+	blimp.position.y = $points.points[randi()% $points.points.size()].y
+	$Blimps.add_child(blimp)
+	move(blimp)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func move(blimp):
+	var tween= create_tween().set_trans(Tween.TRANS_LINEAR)
+
+	tween.tween_property(blimp,
+	'position',
+	Vector2((blimp.position.x+1200)*dir,blimp.position.y),
+	20)
+	
+	await tween.finished
+	blimp.queue_free()
+
+
+func _on_timer_timeout():
+	spawn_blimp()
--- Scenes/UI/converted.gd
+++ Scenes/UI/converted.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var converted_text = get_node("Label")
-@onready var bar = get_node("../Conversion Progress Bar")
+@onready var bar = get_node("../Progress Bar UI/ProgressBar")
 @onready var WOM = get_node("../Convert_WOM")
 @onready var Pamphlet = get_node("../Convert_Pamphlet")
 @onready var Blimp = get_node("../Convert_Blimp")
