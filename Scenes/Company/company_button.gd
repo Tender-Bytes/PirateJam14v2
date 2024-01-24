bee3845e4d05bf39f77ae96beaf51ca8229fa4ad
Brian <brianchan98@hotmail.com>
2024-01-23 23:55:13 -0500

    added MC to overworld, created company button base

---
+++ Scenes/Company/company_button.gd
@@ -0,0 +1,51 @@
+extends TextureButton
+@export var particles : Texture2D
+@export var infographic : Texture2D
+@export var timed = true
+@export var audio=AudioStreamWAV
+
+
+var original_size = scale 
+var grow_size = Vector2(1.06,1.06)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if particles:
+		$emitter.texture = particles
+	else:
+		$emitter.hide()
+	if infographic:
+		$Info.texture=infographic
+	if audio:
+		$sound.stream=audio
+	 # Replace with function body.
+
+
+func _on_mouse_entered():
+	if not(disabled):
+		change_button_size(grow_size,.1)
+		$hover.show()
+
+#WHEN HOVER ASSET FIXED UNCOMMENT THIS
+#func _on_hover_mouse_exited():
+	#change_button_size(original_size,.1)
+	#$hover.hide()
+
+#CAN DELETE WHEN HOVER ASSET FIXED
+func _on_mouse_exited():
+	change_button_size(original_size,.1)
+	$hover.hide()
+
+
+func change_button_size(size:Vector2, duration:float):
+	var tween= create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self,'scale',size,duration)
+
+
+func _on_pressed():
+	$emitter.position=get_local_mouse_position()
+	$emitter.emitting=true
+	$sound.play()
+	if timed:
+		self.disabled = true
+
