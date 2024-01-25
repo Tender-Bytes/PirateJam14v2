873977b88504ca0fcf3853e4b77db85cda2baf9d
theonekami <theonekamisama@gmail.com>
2024-01-25 17:02:33 +0530

    hover fix and gener stuff

--- Scenes/UI/progress_bar.gd
+++ Scenes/UI/progress_bar.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var bar = get_node("ProgressBar")
 
 signal converted_pop(value: float)
-
+signal victory
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	bar.value = 0
@@ -16,3 +16,5 @@ func _process(delta):
 
 func charge_progress_bar(pop):
 	bar.value = pop
+	if((pop>9900)):
+		get_tree().change_scene_to_file("res://Scenes/kami_scenes/kami_end_screen.tscn")
