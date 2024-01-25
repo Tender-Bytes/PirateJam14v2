1ce4904c27022b1b38188f5995b8d5e611991418
theonekami <theonekamisama@gmail.com>
2024-01-26 00:46:01 +0530

    er

--- Endscreen.gd
+++ Endscreen.gd
@@ -1,17 +1,33 @@
 extends Node2D
 
 @onready var words = $story.text
+
+@export var beg:Array[String]
+
+@onready var t= beg.size()
+@onready var r = 0
+var tween
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("begi ")
-	await $AnimationPlayer.animation_finished
-	var tween= create_tween().set_trans(Tween.TRANS_LINEAR).set_parallel()
-	var x= words.length()
-#	tween.tween_property($Image,"self_modulate",Color("white",255.0),1)
-	tween.tween_property($story,"visible_ratio",1,x*0.04)
-	pass # Replace with function body.
+    $AnimationPlayer.play("begi ")
+    await $AnimationPlayer.animation_finished
 
+    
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
+
+
+func _on_button_pressed():
+    $AudioStreamPlayer2D.play()
+    var tween= create_tween().set_trans(Tween.TRANS_LINEAR)
+    tween.tween_property($VBoxContainer/TextureRect,"modulate",Color("red",120.0),1)
+    $Timer.start()
+    await $Timer.timeout
+    var tween2 =create_tween().set_trans(Tween.TRANS_LINEAR).set_parallel(true)
+    tween2.tween_property($ColorRect2,"modulate",Color("white",255.0),3)
+    tween2.tween_property($ColorRect2,"color",Color("black",255),3)
+    await tween2.finished
+    get_tree().change_scene_to_file("res://Scenes/kami_scenes/pirate_game_spalsh.tscn")
+    
---
+++ Scenes/kami_scenes/kami_end_real.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+@export var imgs:Array[Texture2D]
+@export var lines:Array[String]
+@export var number_of_slides: int
+
+@onready var base_cutscene=preload("res://Scenes/kami_scenes/kami_cutscene.tscn")
+
+@onready var positionsss= Vector2(357,197)
+@onready var current=0
+@onready var t
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	await $Timer.timeout
+	var tween= create_tween().set_trans(Tween.TRANS_LINEAR).set_parallel()
+	tween.tween_property($Menu_art_1,"self_modulate",Color("193051"),1)
+	tween.tween_property($Menu_art_2,"self_modulate",Color("193051"),1)
+	print(lines)
+	play_cutscene(0)
+	 # Replace with function body.
+
+func play_cutscene(i):
+	var y =base_cutscene.instantiate()
+	y.words=lines[i]
+	y.img=imgs[i]
+	y.position=positionsss
+	$scenesss.add_child(y)
+	y.next.connect(_on_cut_scene_next)
+	t=y
+	current+=1
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
+
+func _on_skip_skip():
+	get_tree().change_scene_to_file("res://Scenes/Levels/overworld.tscn") # Replace with function body.
+
+
+func _on_cut_scene_next():
+	t.queue_free()
+	if current>=number_of_slides:
+		print("onto game")
+		get_tree().change_scene_to_file("res://Scenes/Levels/overworld.tscn")
+	else:
+		play_cutscene(current)
